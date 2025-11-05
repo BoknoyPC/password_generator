f8294c0a100712d3fb068c1179777676e763f0a1
Pedrick <pedrickcapuno@gmail.com>
2025-11-05 16:31:57 +0800

    changes

---
+++ password_generator/password_generator.py
@@ -0,0 +1,16 @@
+import random
+
+def main():
+    ask_username = input("Please enter your username: ")
+
+    ask_password = int(input("Please enter how many characters do you want for password: "))
+
+
+def creating_password(r):
+
+
+
+
+
+if __name__=="__main__":
+    main()
