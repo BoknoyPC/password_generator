7f7ff6a29348359713e264483e4cec3db4a8dd7e
Pedrick <pedrickcapuno@gmail.com>
2025-11-05 16:30:39 +0800

    changes

--- password_generator/test.py
+++
@@ -1,9 +0,0 @@
-#this is a test
-print("Hello World")
-print("Bye World")
-print("Bye World")
-
-#ang obob mag paliwanag ng staging
-#ang obob mag paliwanag ng staging potek
-
-#ang obob mag paliwanag ng staging potek talaga
