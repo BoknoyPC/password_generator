77f2333ecfbf80f9ae975080fac4b463dfcc3e4c
Pedrick <pedrickcapuno@gmail.com>
2025-11-10 16:47:40 +0800

    update

--- password_generator/password_generator.py
+++ password_generator/password_generator.py
@@ -1,15 +1,22 @@
 import random
+import string
 
 def main():
     ask_username = input("Please enter your username: ")
 
     ask_password = int(input("Please enter how many characters do you want for password: "))
 
+    password = creating_password(ask_password)
 
-#def creating_password(r):
+    print(f"Hello your username is : {ask_username}, your generated password is : {password}")
 
 
+def creating_password(pw):
+    characters = string.ascii_letters + string.digits + string.punctuation     
 
+    password =''.join(random.choice(characters) for _ in range(pw))
+
+    return password
 
 
 if __name__=="__main__":
