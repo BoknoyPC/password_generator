cd4444ce054be77037dce776777bd51c7aaede62
Pedrick <pedrickcapuno@gmail.com>
2025-11-12 16:25:44 +0800

    updates

--- password_generator/password_generator.py
+++ password_generator/password_generator.py
@@ -1,8 +1,11 @@
 import random
 import string
+import csv
 
 def main():
     ask_username = input("Please enter your username: ")
+
+    ask_website = input("Please enter which site you plan to use it: ")
 
     while True:
         try:
@@ -16,10 +19,14 @@ def main():
         except ValueError:
             print("Invalid input, Please enter a number")
         
-
     password = creating_password(ask_password)
 
-    print(f"Hello : {ask_username}, your generated password is : {password}")
+
+    with open("credential.txt","a") as file:
+        file.write(f"Hello {ask_username}, your password is: {password}\n")
+        file.write(f"This is going to be used for {ask_website}\n\n")
+
+    print(f"{ask_username} your password is now generated")
 
 
 def creating_password(pw):
